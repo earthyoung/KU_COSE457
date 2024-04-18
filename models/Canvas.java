d804347aa835dfa416ff57f30895761f68e4631b
abpo1 <45586900+abpo1@users.noreply.github.com>
2024-04-18 17:26:58 +0900

    feat: add observer pattern

--- models/Canvas.java
+++ models/Canvas.java
@@ -1,7 +1,20 @@
 package models;
-
-public abstract class Canvas {
-
+import java.util.List;
+import java.util.ArrayList;
+public abstract class Canvas implements Subject{
+	class Message{
+		int x,y,width,height;
+		public Message(int x,int y,int width,int height) {
+			this.x=x;
+			this.y=y;
+			this.width=width;
+			this.height=height;
+		}
+	}
+	private List<Observer> observers;
+	private Message message;
+	
+	
     protected int x;
     protected int y;
     protected int width;
@@ -9,11 +22,31 @@ public abstract class Canvas {
 
     public Canvas() {}
     public Canvas(int x, int y, int width, int height) {
-        this.x = x;
+        this.observers=new ArrayList<>();
+    	this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.message = new Message(x,y,width,height);
     }
+    
+    @Override
+    public void register(Observer obj) {
+    	if(!observers.contains(obj))observers.add(obj);
+    }
+    
+    @Override
+    public void unregister(Observer obj){
+    	observers.remove(obj);
+    }
+    
+    @Override
+    public void notifyObservers() {
+    	for(Observer observer:observers) {
+    		observer.update(message.x,message.y,message.width,message.height);
+    	}
+    }
+
 
     public int getX() {
         return this.x;
@@ -33,18 +66,26 @@ public abstract class Canvas {
 
     public void setX(int x) {
         this.x = x;
+        this.message.x = x;
+        notifyObservers();
     }
 
     public void setY(int y) {
         this.y = y;
+        this.message.y = y;
+        notifyObservers();
     }
 
     public void setWidth(int width) {
         this.width = width;
+        this.message.width = width;
+        notifyObservers();
     }
 
     public void setHeight(int height) {
         this.height = height;
+        this.message.height = height;
+        notifyObservers();
     }
 
 }
