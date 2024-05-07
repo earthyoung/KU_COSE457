a698be0c95ac37cc7f70c02bdda863364a9eea94
abpo1 <45586900+abpo1@users.noreply.github.com>
2024-05-07 21:09:38 +0900

    feat: settingArea 구현

---
+++ controller/TLine.java
@@ -0,0 +1,30 @@
+package models;
+
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+
+public class TLine extends TShape {
+
+    public TLine() {
+        this.shape = new Line2D.Double();
+    }
+
+    @Override
+    public TShape clone() {
+        return new TLine();
+    }
+
+    @Override
+    public void prepareDrawing(int x, int y) {
+        Line2D line = (Line2D) this.shape;
+        line.setLine(x,y,x,y);
+    }
+
+    @Override
+    public void keepDrawing(int x, int y) {
+        Line2D line = (Line2D) this.shape;
+        line.setLine(line.getX1(),line.getY1(),x,y);
+    }
+    
+    
+}
