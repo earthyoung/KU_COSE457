8d2ff1617775f462942d9f302fa48d61a2c8f34f
abpo1 <45586900+abpo1@users.noreply.github.com>
2024-05-07 20:56:24 +0900

    feat: settingArea 구현
    observer pattern을 사용하여 TShape를 설정창과 연동함.

--- controller/SetSizeCommand.java
+++ controller/SetSizeCommand.java
@@ -2,19 +2,19 @@ package controller;
 
 import models.*;
 
-public class SetSizeCommand implements Command {
+public abstract class SetSizeCommand implements Command {
 	
-	Canvas cans;
-	int cWidth,cHeight;
-	
-	public SetSizeCommand(int width,int height,Canvas canvas) {
-		cans=canvas;
-		cWidth=width;
-		cHeight=height;
-	}
-	
-	public void execute() {
-		cans.setHeight(cWidth);
-		cans.setWidth(cHeight);
-	}
+//	TShape cans;
+//	int cWidth,cHeight;
+//
+//	public SetSizeCommand(int width, int height, TShape TShape) {
+//		cans= TShape;
+//		cWidth=width;
+//		cHeight=height;
+//	}
+//
+//	public void execute() {
+//		cans.setHeight(cWidth);
+//		cans.setWidth(cHeight);
+//	}
 }
--- controller/SettingArea.java
+++ controller/SettingArea.java
@@ -1,25 +1,79 @@
 package controller;
 
-import models.*;
-import java.awt.Color;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SettingArea implements Observer{
+import models.Observable;
+import models.Observer;
+import models.TShape;
+
+public class SettingArea extends JPanel implements Observer{
 	
-	private SettingArea() {}
-	
-	private static class singletonHelper{
-		private static final SettingArea INSTANCE=new SettingArea();
-	}
+private static final long serialVersionUID = 1L;
+
+	private static SettingArea instance;
+
 	public static SettingArea getInstance() {
-		return singletonHelper.INSTANCE;
+		if (instance == null) {
+			instance = new SettingArea();
+		}
+		return instance;
 	}
-	
-	@Override
-	public void update(int x,int y,int width,int height) {
-	//update when size changed
-		
-		
-	}
-	
-}
 
+    private JTextField sizeField;
+    private Color currentColor;
+    private JCheckBox check;
+   
+    private SettingArea() {
+ 
+        this.setLayout(new GridLayout(15,2));
+        //this.setLayout(new FlowLayout(FlowLayout.LEFT));
+        
+        this.add(new JLabel("색:"));
+        JButton colorPickerButton = new JButton("색상 선택");
+        colorPickerButton.addActionListener(e -> {
+            Color selectedColor = JColorChooser.showDialog(null, "색상 선택", currentColor); // 초기 색상을 검은색으로 설정
+            if (selectedColor != null) {
+                // 여기에서 선택된 색상을 사용하는 로직을 구현합니다.
+            	
+            }
+        });
+        this.add(colorPickerButton);
+       
+        this.add(new JLabel("크기:"));
+        sizeField = new JTextField("100");
+        sizeField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                changeSize();
+            }
+        });
+        this.add(sizeField);
+        
+        check= new JCheckBox("선택",true);
+        this.add(check);
+    }
+
+    private void changeColor() {
+       
+    }
+
+    private void changeSize() {
+       
+    }
+    
+	@Override
+	public void update(Observable o) {
+		if(o==null)return;
+		//TShape가 업데이트 되었을 때 해당 객체의 값을 받아옴
+		TShape t=(TShape)o;
+		
+		sizeField.setText(""+t.getSize());
+		currentColor=t.getShapeColor();
+		check.setSelected(t.isSelected());
+	}
+    
+
+}
